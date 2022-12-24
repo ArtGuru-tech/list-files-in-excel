1635eb4f66d8ce3fc3d2f5cc1379e625cf5354b0
Melek Gharbi <gharbimelek92@gmail.com>
2022-12-24 18:51:35 +0100

    stable export with client name fix

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 import os
 import csv
 
-def list_files(folder):
+def list_files(root_dir):
     file_list = []
-    for root, dirs, files in os.walk(folder):
+
+    # Walk through all subdirectories and files
+    for root, dirs, files in os.walk(root_dir):
+        # Get the client name from the first level subfolder
+        client_name = os.path.basename(os.path.dirname(root))
+
+        # Get the week number and delivery number from the second and third level subfolders
+        week_number = os.path.basename(os.path.dirname(root))
+        delivery_number = os.path.basename(root)
+
+        # Add the file name, client name, week number, delivery number, and full path
+        # to the file to the file list
         for file in files:
-            file_list.append(os.path.join(root, file))
+            client_name = os.path.relpath(os.path.join(root, file), root_dir).split("\\", 1)[0]
+            file_list.append([file, client_name, week_number, delivery_number, os.path.relpath(os.path.join(root, file), root_dir)])
+
     return file_list
 
 def write_to_csv(file_list, csv_file):
     with open(csv_file, "w", newline="") as f:
         writer = csv.writer(f)
-        writer.writerows([[f] for f in file_list])
+        # Add a header row
+        writer.writerow(["Filename", "Client Name", "Week Number", "Delivery Number", "Path"])
+        writer.writerows(file_list)
 
-folder = '/path/to/folder'
-csv_file = '/path/to/output.csv'
+root_dir = r'C:\Users\mega\Documents\sandbox\list-files-in-excel\drive-download-20221224T164507Z-002'
+csv_file = r'C:\Users\mega\Documents\sandbox\list-files-in-excel\output\output.csv'
 
-file_list = list_files(folder)
+file_list = list_files(root_dir)
 write_to_csv(file_list, csv_file)
